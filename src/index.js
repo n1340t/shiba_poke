b7a56c295ede69e5b2b27030c6794ebe511ddaa9
btran <nguyenbao1003@outlook.com>
2022-01-15 20:49:58 -0500

    Update menu and copyright

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import 'bootstrap/js/dist/collapse';
 import '../scss/index.scss';
 import '../asset/images/logo.png';
-import '../asset/images/coming_soon.png';
+import '../asset/images/menu.gif';
 // Close navbar menu on mobile
 $('a.nav-link').on('click', function() {
   $('button.navbar-toggler').trigger('click');
+});
+
+// copyright
+const date = new Date();
+$('footer p').html(function(index, oldStr) {
+  return oldStr.replace(/\d{4}/, date.getFullYear());
 });
